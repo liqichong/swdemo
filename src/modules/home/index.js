8b5ef4b226ca28c9d16b7f4c0aad6edfaf33c611
coiorz <coiorz1989@gmail.com>
2018-06-03 15:41:52 +0800

    request and process detail info

--- src/modules/home/index.js
+++ src/modules/home/index.js
@@ -5,22 +5,38 @@ import People from './pages/people';
 export default {
   namespace : 'home',
   state     : {
-    list   : {},  // {count, 1: {page, count, prev, next people: [1,2,3,...]}
-    people : {},  // {1: {id, name, ...}}
+    list      : {},  // {count, 1: {page, count, prev, next, people: [1,2,3,...]}, ...}
+    people    : {},  // {1: {id, name, ...}, ...}
+    planets   : {},  // {1: 'name', ...}
+    films     : {},
+    species   : {},
+    vehicles  : {},
+    starships : {},
   },
   effects: {
     *fetchList({ payload }, { call, put }) {
-      const { data } = yield call(services.list, payload);
-      // transform response from server 
-      let list = { page: payload, count: data.count, prev: data.previous, next: data.next, people: [] };
-      let people = {};
-      data.results.forEach(p => {
-        p.id = getId(p);
-        list.people.push(p.id);
-        people[p.id] = p;
-      });
+      const { people, list } = yield call(services.list, payload);
       yield put({ type: 'home/savePeople', payload: people });
       yield put({ type: 'home/saveList', payload: list });
+    },
+    *fetchPeople({ payload }, { call, put, select, all }) {
+      let people = yield select(state => state.home.people[payload]);
+      if(!people) {
+        people = yield call(services.people, payload);
+        yield put({ type: 'home/savePeople', payload: { [people.id]: people } });
+      }
+      const { homeworld, films, species, vehicles, starships } = yield all({
+        homeworld : call(services.detail, 'planets', people.homeworld),
+        films     : people.films.map(v => call(services.detail, 'films', v)),
+        species   : people.species.map(v => call(services.detail, 'species', v)),
+        vehicles  : people.vehicles.map(v => call(services.detail, 'vehicles', v)),
+        starships : people.starships.map(v => call(services.detail, 'starships', v)),
+      });
+      yield put({ type: 'home/savePlanet', payload: { [people.homeworld]: homeworld } });
+      yield put({ type: 'home/saveFilms', payload: people.films.reduce((m, v, i) => { m[v] = films[i]; return m; }, {}) });
+      yield put({ type: 'home/saveSpecies', payload: people.species.reduce((m, v, i) => { m[v] = species[i]; return m; }, {}) });
+      yield put({ type: 'home/saveVehicles', payload: people.vehicles.reduce((m, v, i) => { m[v] = vehicles[i]; return m; }, {}) });
+      yield put({ type: 'home/saveStarships', payload: people.starships.reduce((m, v, i) => { m[v] = starships[i]; return m; }, {}) });
     },
   },
   reducers: {
@@ -37,10 +53,37 @@ export default {
     savePeople(state, { payload }) {
       return {
         ...state,
-        people: {
-          ...state.people,
-          ...payload,
-        },
+        people: { ...state.people, ...payload },
+      };
+    },
+    savePlanet(state, { payload }) {
+      return {
+        ...state,
+        planets: { ...state.planets, ...payload },
+      };
+    },
+    saveFilms(state, { payload }) {
+      return {
+        ...state,
+        films: { ...state.films, ...payload },
+      };
+    },
+    saveSpecies(state, { payload }) {
+      return {
+        ...state,
+        species: { ...state.species, ...payload },
+      };
+    },
+    saveVehicles(state, { payload }) {
+      return {
+        ...state,
+        vehicles: { ...state.vehicles, ...payload },
+      };
+    },
+    saveStarships(state, { payload }) {
+      return {
+        ...state,
+        starships: { ...state.starships, ...payload },
       };
     },
   },
@@ -50,7 +93,3 @@ export default {
     '/p/:id' : { component: People },
   },
 };
-
-function getId(result, prop = 'url') {
-  return result[prop] && result[prop].match(/\/(\d+)\/$/)[1]|0;
-}
--- src/modules/home/services.js
+++ src/modules/home/services.js
@@ -3,4 +3,35 @@ import { cachify } from '../../utils';
 
 const API = 'https://swapi.co/api';
 
-export const list = cachify(page => axios.get(`${API}/people/?page=${page}`));
+export const list = cachify(page => axios.get(`${API}/people/?page=${page}`).then(processList.bind(this, page)));
+export const people = cachify(id => axios.get(`${API}/people/${id}`).then(processPeople));
+export const detail = cachify((prop, id) => axios.get(`${API}/${prop}/${id}`).then(getName));
+
+function processList(page, { data }) {
+  let list = { page, count: data.count, prev: data.previous, next: data.next, people: [] };
+  let people = {};
+  data.results.forEach(p => {
+    p = processPeople({ data: p });
+    list.people.push(p.id);
+    people[p.id] = p;
+  });
+  return { list, people };
+}
+
+function processPeople({ data }) {
+  data.id = getId(data.url);
+  data.homeworld = getId(data.homeworld);
+  data.films = data.films.map(getId);
+  data.species = data.species.map(getId);
+  data.vehicles = data.vehicles.map(getId);
+  data.starships = data.starships.map(getId);
+  return data;
+}
+
+function getName({ data }) {
+  return data.name || data.title;
+}
+
+function getId(str) {
+  return parseInt(str.match(/\/(\d+)\/$/)[1]);
+}
