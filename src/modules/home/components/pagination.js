5e1f31d674a6a293c6e8e81e091e4a475441fa5c
coiorz <coiorz1989@gmail.com>
2018-06-03 17:17:32 +0800

    fix page num

--- src/modules/home/components/pagination.js
+++ src/modules/home/components/pagination.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default ({ page, count, prev, next }) => (
   <div className='text-center pt-3'>
     {prev && <Link to={`/${parseInt(page) - 1}`}>&lt;prev</Link>}
-    <span className='ml-4 mr-4'>{page} / {count}</span>
+    <span className='ml-4 mr-4'>{page} / {Math.ceil(count / 10)}</span>
     {next && <Link to={`/${parseInt(page) + 1}`}>next&gt;</Link>}
   </div>
 );
